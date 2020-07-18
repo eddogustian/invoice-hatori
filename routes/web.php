f8cd84a82cbe001751573d915230810f6920dec6
eddogustian <eddogustian78@gmail.com>
2020-07-18 21:15:09 +0700

    MASTER

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'product'], function() {
+    Route::get('/', 'ProductController@index');
+});
+// Penjelasan: Agar lebih rapi, kita masukkan kedalam group sehingga apapun routing yang berada dalam group tersebut akan memiliki prefix atau awalan /product
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
